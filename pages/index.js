1e000655dec572918910aa4e7d5633d81735ea32
sajib925 <sajibahmed1004@gmail.com>
2022-12-18 11:35:13 +0600

    cta problem fixed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import About__us__section from "../components/AboutUs/About__us__section";
 import Blog_section from "../components/Blog_Section/Blog_section";
-import CTA__SECTION from "../components/CTA/CTA__SECTION";
 import Feedback_section from "../components/Feedback_section/Feedback_section";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/header/header";
@@ -11,6 +10,7 @@ import Our_work_process from "../components/Our_work_process/Our_work_process";
 import Partner__section from "../components/Partner/Partner__section";
 import Rating_section from "../components/Rating/Rating_section";
 import Team from "../components/Team/Team";
+import CTA__SECTION from './../components/CTA/CTA__section';
 
 
 export default function Home() {
